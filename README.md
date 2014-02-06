515cfde7a9478c69c077f1fb84f49fa16e2cd62b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-02-07 08:41:09 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Localizer - It's new $module
+
+# SYNOPSIS
+
+    use Localizer;
+
+# DESCRIPTION
+
+Localizer is ...
+
+# LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tokuhiro Matsuno <tokuhirom@gmail.com>
---
+++ lib/Localizer.pm
@@ -0,0 +1,39 @@
+package Localizer;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Localizer - It's new $module
+
+=head1 SYNOPSIS
+
+    use Localizer;
+
+=head1 DESCRIPTION
+
+Localizer is ...
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
