63cf5a67f78b8c5692b9400b2f3d86f8258bbcd4
moznion <moznion@gmail.com>
2014-02-09 22:35:19 +0900

    Replace L::MT::L::Gettext::_gettext_to_maketext to self impl

--- lib/Localizer/Format/Gettext.pm
+++ lib/Localizer/Format/Gettext.pm
@@ -5,12 +5,33 @@ use utf8;
 use 5.010_001;
 use parent qw(Localizer::Format::Maketext);
 
-use Locale::Maketext::Lexicon::Gettext ();
-
 sub compile {
     my ($self, $fmt) = @_;
-    $fmt = Locale::Maketext::Lexicon::Gettext::_gettext_to_maketext($fmt);
+    $fmt = $self->_gettext_to_maketext($fmt);
     return $self->SUPER::compile($fmt);
+}
+
+# Steal from Maketext:Lexicon::Gettext::_gettext_to_maketext
+sub _gettext_to_maketext {
+    my ($self, $str) = @_;
+
+    $str =~ s{([\~\[\]])}{~$1}g;
+    $str =~ s{
+        ([%\\]%)                        # 1 - escaped sequence
+    |
+        %   (?:
+                ([A-Za-z#*]\w*)         # 2 - function call
+                    \(([^\)]*)\)        # 3 - arguments
+            |
+                ([1-9]\d*|\*)           # 4 - variable
+            )
+    }{
+        $1 ? $1
+           : $2 ? "\[$2,"._unescape($3)."]"
+                : "[_$4]"
+    }egx;
+
+    return $str;
 }
 
 1;
