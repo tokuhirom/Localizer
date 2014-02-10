13211cb24bb3b7ae88c198420f89065531a88fa5
moznion <moznion@gmail.com>
2014-02-11 00:44:56 +0900

    Self implement compile for gettext

--- lib/Localizer/Style/Gettext.pm
+++ lib/Localizer/Style/Gettext.pm
@@ -7,39 +7,94 @@ use parent qw(Localizer::Style::Maketext);
 
 sub compile {
     my ($self, $fmt) = @_;
-    $fmt = $self->_gettext_to_maketext($fmt);
-    return $self->SUPER::compile($fmt);
+    my $code = $self->_compile($fmt);
+    return $code;
 }
 
-# Steal from Locale::Maketext::Lexicon::Gettext::_gettext_to_maketext
-sub _gettext_to_maketext {
+sub _compile {
     my ($self, $str) = @_;
 
-    $str =~ s{([\~\[\]])}{~$1}g;
-    $str =~ s{
-        ([%\\]%)                        # 1 - escaped sequence
-    |
-        %   (?:
-                ([A-Za-z#*]\w*)         # 2 - function call
-                    \(([^\)]*)\)        # 3 - arguments
-            |
-                ([1-9]\d*|\*)           # 4 - variable
+    my @code;
+    while ($str =~ m/
+            (.*?)
+            (
+                (%%)
+                |
+                %(?:
+                    ([A-Za-z#*]\w*)\(([^\)]*)\)
+                    |
+                    ([1-9]\d*|\*)
+                )
+                |
+                (\\%)
+                |
+                $
             )
-    }{
-        $1 ? $1
-           : $2 ? "\[$2,".$self->_unescape($3)."]"
-                : "[_$4]"
-    }egx;
+        /gsx
+    ) {
+        if ($1) {
+            my $text = $1;
+            $text =~ s/\\/\\\\/g;
+            push @code, "'" . $text . "',";
+        }
+        if ($2) {
+            if ($3) {
+                my $text = $3;
+                push @code, "'" . $3 . "',";
+            }
+            elsif ($4) {
+                my $function_name = $4;
+                if ($function_name eq '*') {
+                    $function_name = 'quant';
+                }
+                elsif ($function_name eq '#') {
+                    $function_name = 'numf';
+                }
 
-    return $str;
-}
+                my $code = q{$_[0]->call_function('} . $function_name . q{', };
+                for my $arg (split(/,/, $5)) {
+                    if (my $num = $arg =~ /%(.+)/) {
+                        $code .= '$_[' . $num . '], ';
+                    }
+                    else {
+                        $code .= "'" . $arg . "', ";
+                    }
+                }
+                $code .= '), ';
+                push @code, $code;
+            }
+            elsif ($6) {
+                my $arg = $6;
 
-# Steal from Locale::Maketext:Lexicon::Gettext::_unescape
-sub _unescape {
-    my ($self, $str) = @_;
-    join( ',',
-        map { /\A(\s*)%([1-9]\d*|\*)(\s*)\z/ ? "$1_$2$3" : $_ }
-            split( /,/, $str ) );
+                my $var = '';
+                if ($arg eq '*') {
+                    $var = '@_[1 .. $#_],';
+                }
+                else {
+                    $var = '$_[' . $arg . '],';
+                }
+                push @code, $var;
+            }
+            elsif ($7) {
+                my $text = $7;
+                $text =~ s/\\/\\\\\\\\/g;
+                push @code, "'" . $text . "',";
+            }
+        }
+    }
+
+    if(@code == 0) { # not possible?
+        return \'';
+    }
+    elsif(@code > 1) { # most cases, presumably!
+        unshift @code, "join '',\n";
+    }
+    unshift @code, "use strict; sub {\n";
+    push @code, "}\n";
+
+    my $sub = eval(join '', @code); ## no critic.
+    die "$@ while evalling" . join('', @code) if $@; # Should be impossible.
+    return $sub;
 }
 
 1;
