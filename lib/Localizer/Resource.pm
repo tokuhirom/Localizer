441152a1ddc7c883d349018899c50449f96d0ec9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-02-11 10:26:58 +0900

    Split built-in functions package.

--- lib/Localizer/Resource.pm
+++ lib/Localizer/Resource.pm
@@ -5,51 +5,13 @@ use utf8;
 use 5.010_001;
 
 use Localizer::Style::Gettext;
+use Localizer::BuiltinFunctions;
 
-our $BUILTIN_FUNCTIONS;
-$BUILTIN_FUNCTIONS = {
-    sprintf  => sub { sprintf(shift, @_) },
-    numf     => sub { # imported by Locale::Maketext
-        my($num) = @_[0,1];
-        if($num < 10_000_000_000 and $num > -10_000_000_000 and $num == int($num)) {
-            $num += 0;  # Just use normal integer stringification.
-            # Specifically, don't let %G turn ten million into 1E+007
-        }
-        else {
-            $num = CORE::sprintf('%G', $num);
-            # "CORE::" is there to avoid confusion with the above sub sprintf.
-        }
-        while( $num =~ s/^([-+]?\d+)(\d{3})/$1,$2/s ) {1}  # right from perlfaq5
-        # The initial \d+ gobbles as many digits as it can, and then we
-        #  backtrack so it un-eats the rightmost three, and then we
-        #  insert the comma there.
-
-        return $num;
-    },
-    numerate => sub { # imported by Locale::Maketext
-        # return this lexical item in a form appropriate to this number
-        my($num, @forms) = @_;
-        my $s = ($num == 1);
-
-        return '' unless @forms;
-        if(@forms == 1) { # only the headword form specified
-            return $s ? $forms[0] : ($forms[0] . 's'); # very cheap hack.
-        }
-        else { # sing and plural were specified
-            return $s ? $forms[0] : $forms[1];
-        }
-    },
-    quant => sub { # imported by Locale::Maketext
-        my($num, @forms) = @_;
-
-        return $num if @forms == 0; # what should this mean?
-        return $forms[2] if @forms > 2 and $num == 0; # special zeroth case
-
-        # Normal case:
-        # Note that the formatting of $num is preserved.
-        return( $BUILTIN_FUNCTIONS->{numf}->($num) . ' ' . $BUILTIN_FUNCTIONS->{numerate}->($num, @forms) );
-        # Most human languages put the number phrase before the qualified phrase.
-    },
+our $BUILTIN_FUNCTIONS = {
+    numf     => \&Localizer::BuiltinFunctions::numf,
+    numerate => \&Localizer::BuiltinFunctions::numerate,
+    quant    => \&Localizer::BuiltinFunctions::quant,
+    sprintf  => \&Localizer::BuiltinFunctions::sprintf,
 };
 
 use Mouse;
