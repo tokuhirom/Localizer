e689201c09932e75b263191fde1e9ce468f2e698
moznion <moznion@gmail.com>
2014-02-16 16:54:19 +0900

    Add doc for L::Dict

--- lib/Localizer/Dictionary.pm
+++ lib/Localizer/Dictionary.pm
@@ -26,3 +26,43 @@ sub add_entry_position {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Localizer::Dictionary - Dictionary for Localizer::Scanner
+
+=head1 DESCRIPTION
+
+Dictionary for Localizer::Scanner class.
+
+=head1 SYNOPSIS
+
+    use Localizer::Dictionary;
+
+    my $dict = Localizer::Dictionary->new();
+    $dict->add_entry_position('Hi %1', 'foo.tt', 10);
+    $dict->exists_msgid('Hi %1');  # => 1
+    $dict->exists_msgid('foobar'); # => ''
+
+=head1 METHODS
+
+=over 4
+
+=item * Localizer::Dictionary->new()
+
+Constructor. It makes dictionary instance.
+
+=item * $dictionary->add_entry_position($msgid, $file, $line)
+
+Add entry into dictionary. C<$msgid> is the ID of message (similar to key of dictionary of L<Localizer::Resource>),
+C<$file> is the location of file, and C<$line> is the line number where there is C<$msgid>.
+
+=item * $dictionary->exists_msgid($msgid)
+
+Returns whether C<$msgid> is registered in the dictionary.
+If it exists, this method returns true value, otherwise it returns false value.
+
+=back
